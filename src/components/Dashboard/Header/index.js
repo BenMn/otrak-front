ad1daa4b6aa74ad4bc83cfdd1c237a736dc4e005
lea-lp <lea.leipp@gmail.com>
2019-09-12 17:59:29 +0200

    Folder oragnisation Header Dashboard

---
+++ src/components/Dashboard/Header/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {
+  Grid,
+} from '@material-ui/core';
+
+import {
+  DashboardHeaderConainer,
+} from 'src/styles/materialUi/materialUiStyles/Dashboard';
+
+import UserParams from './UserParams';
+import UserInfos from './UserInfos';
+import ConfigButtons from './ConfigButtons';
+
+import '../Dashboard.scss';
+
+const Dashboard = () => (
+  <div id="Dashboard">
+
+    <DashboardHeaderConainer>
+
+      <Grid container align="center" spacing={5}>
+
+        <Grid item lg={4} md={4} xs={12} align="start">
+          <UserParams />
+        </Grid>
+
+        <Grid item lg={4} md={4} xs={12}>
+          <UserInfos />
+        </Grid>
+
+        <Grid item lg={4} md={4} xs={12} align="end">
+          <ConfigButtons />
+        </Grid>
+
+      </Grid>
+
+    </DashboardHeaderConainer>
+
+  </div>
+);
+
+export default Dashboard;
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,42 +1,10 @@
 import React from 'react';
 
-import {
-  Grid,
-} from '@material-ui/core';
-
-import {
-  DashboardHeaderConainer,
-} from 'src/styles/materialUi/materialUiStyles/Dashboard';
-
-import UserParams from './UserParams';
-import UserInfos from './UserInfos';
-import ConfigButtons from './ConfigButtons';
-
-import './Dashboard.scss';
+import Hearder from './Header';
 
 const Dashboard = () => (
   <div id="Dashboard">
-
-    <DashboardHeaderConainer maxWidth="lg">
-
-      <Grid container align="center" spacing={5}>
-
-        <Grid item lg={4} md={4} xs={12} align="start">
-          <UserParams />
-        </Grid>
-
-        <Grid item lg={4} md={4} xs={12}>
-          <UserInfos />
-        </Grid>
-
-        <Grid item lg={4} md={4} xs={12} align="end">
-          <ConfigButtons />
-        </Grid>
-
-      </Grid>
-
-    </DashboardHeaderConainer>
-
+    <Hearder />
   </div>
 );
 
