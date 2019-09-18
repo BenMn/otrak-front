b0bb0a2818e98232d183a76f8fe3798acfd39330
lea-lp <lea.leipp@gmail.com>
2019-09-18 19:01:22 +0200

    ComponentDidMount removed in dashboard index

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -9,51 +9,38 @@ import Tabs from './Tabs';
 
 import './Dashboard.scss';
 
-class Dashboard extends React.Component {
-  componentDidMount() {
-    const { fetchUserProfileInfos, userAuthToken } = this.props;
-    fetchUserProfileInfos(userAuthToken);
-  }
-
-  render() {
-    const {
-      avatarUploadHandler,
-      handleUsernameInput,
-      handleUsernameInputSubmit,
-      userAuthInfos,
-      handleNewUsername,
-      handleOpen,
-      userInfos,
-    } = this.props;
-    return (
-      <div id="Dashboard">
-        <Header
-          avatarUploadHandler={avatarUploadHandler}
-          handleUsernameInput={handleUsernameInput}
-          handleUsernameInputSubmit={handleUsernameInputSubmit}
-          userAuthInfos={userAuthInfos}
-          handleNewUsername={handleNewUsername}
-          handleOpen={handleOpen}
-          userInfos={userInfos}
-        />
-        <Tabs />
-      </div>
-    );
-  }
-}
+const Dashboard = ({
+  avatarUploadHandler,
+  handleUsernameInput,
+  handleUsernameInputSubmit,
+  userAuthInfos,
+  handleNewUsername,
+  handleOpen,
+  userInfos,
+}) => (
+  <div id="Dashboard">
+    <Header
+      avatarUploadHandler={avatarUploadHandler}
+      handleUsernameInput={handleUsernameInput}
+      handleUsernameInputSubmit={handleUsernameInputSubmit}
+      userAuthInfos={userAuthInfos}
+      handleNewUsername={handleNewUsername}
+      handleOpen={handleOpen}
+      userInfos={userInfos}
+    />
+    <Tabs />
+  </div>
+);
 
 Dashboard.propTypes = {
   avatarUploadHandler: PropTypes.func.isRequired,
   handleUsernameInput: PropTypes.func.isRequired,
   handleUsernameInputSubmit: PropTypes.func.isRequired,
   handleOpen: PropTypes.func.isRequired,
-  fetchUserProfileInfos: PropTypes.func.isRequired,
-  userAuthToken: PropTypes.string,
   userInfos: PropTypes.object,
 };
 
 Dashboard.defaultProps = {
-  userAuthToken: '',
   userInfos: {},
 };
 
